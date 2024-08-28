bb20c6b721e87e3fdcf234b256072b553a0b4d24
AbulFozolJumman <abulfozoljumman@gmail.com>
2024-08-28 22:53:42 +0600

    All initial setup and installation done

---
+++ src/components/ui/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <img src="/src/assets/favicon.png" alt="logo" />
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import Container from "@/components/ui/Container";
+import Navbar from "@/components/ui/Navbar";
+
+const Home = () => {
+  return (
+    <Container>
+      <Navbar />
+    </Container>
+  );
+};
+
+export default Home;
